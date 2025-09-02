cbe18c8c2f0bb48ac44dc3f1164e0556041b29a4
unknown <Shibss1@shibeyu@gmail.com>
2025-09-02 22:56:16 +0800

    Implemented feature 1 (link checking)

--- app.js
+++ app.js
@@ -57,5 +57,23 @@ app.post('/pending', async (req, res) => {
     }
 });
 
+app.post('/check', async (req, res) => { //For checking if given link is a threat, or just unverified by devs
+    try {
+        const { link } = req.body;
+        const found = await db.collection("malicious").where("description", "==", link).get();
+        const snapshot = found.docs.map(doc => ({
+            id: doc.id,
+            ...doc.data()
+        }));
+        if (found.empty) {
+            res.status(201).send({ message: "Unverified link" });
+        }
+        res.status(201).send({ Threat: snapshot[0].threat }); 
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+
 app.listen(8080, "127.0.0.1");
 console.log("Test thing is running @ 127.0.0.1:8080");
