f616432369d1445f663fbff26fe4620f7998c3f7
LooiLe <looilefire@gmail.com>
2025-09-02 23:20:10 +0800

    final product

--- app.js
+++ app.js
@@ -4,37 +4,42 @@ const db = require('./firebase');
 
 app.use(express.json());
 
-app.get("/test", (req, res) => {
-    const status = {
-        "Status": "Running"
-    };
-    res.send(status);
-});
-
-app.post('/check', async (req, res) => {
+//For checking if given link is a threat, or just unverified by devs
+app.post('/user_check', async (req, res) => {
     try {
-        const { name, type, description, threat } = req.body;
-        const docRef = await db.collection('malicious').add({ name, type, description, threat });
-        res.status(201).send({ message: 'Source added!', id: docRef.id });
-    } catch (error) {
-        res.status(500).send({ error: error.message });
-    }
-});
-
-app.get('/check', async (req, res) => {
-    try {
-        const snapshot = await db.collection('malicious').get();
-        const malicious = snapshot.docs.map(doc => ({
+        const { name } = req.body;
+        const found = await db.collection("malicious").where("name", "==", name).get();
+        const snapshot = found.docs.map(doc => ({
             id: doc.id,
             ...doc.data()
         }));
-        res.status(200).json(malicious);
+        if (found.empty) {
+            res.status(201).send({ message: "Unverified link" });
+        }
+        res.status(201).send({ Threat: snapshot[0].threat }); 
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
 });
 
-app.post('/confirmMalicious', async (req, res) => {
+// update pending when user submit request for reporting
+app.post('/user_report', async (req, res) => {
+    try {
+        const { name, type, description } = req.body;
+        const check_mali = await db.collection('malicious').where("name", "==", name).get();
+        const check_pending = await db.collection('pending').where("name", "==", name).get();
+        if (!check_mali.empty || !check_pending.empty) {
+            res.status(201).send({ message: 'Already added!' });
+        }
+        const docRef = await db.collection('pending').add({ name, type, description});
+        res.status(201).send({ message: 'Pending added!', id: docRef.id });
+    } catch (error) {
+        res.status(500).json({ error: error.message});
+    }
+});
+
+// once admin approve, delete from pending and add to malicious
+app.post('/dev_verify', async (req, res) => {
     try {
         const { id, threat } = req.body;
         const doc = (await db.collection('pending').doc(id).get());
@@ -47,15 +52,18 @@ app.post('/confirmMalicious', async (req, res) => {
     }
 });
 
-app.post('/pending', async (req, res) => {
+// returns pending verification
+app.get('/dev_pending', async (req, res) => {
     try {
-        const { name, type, description } = req.body;
-        const docRef = await db.collection('pending').add({ name, type, description});
-        res.status(201).send({ message: 'pending added!', id: docRef.id });
+        const receive = await db.collection('pending').get();
+        const pending = receive.docs.map(doc => ({
+            id: doc.id,
+            ...doc.data()
+        }));
+        res.status(201).json({ data: pending });
     } catch (error) {
-        res.status(500).json({ error: error.message});
+        res.status(500).json({ error: error.message });
     }
 });
 
 app.listen(8080, "127.0.0.1");
-console.log("Test thing is running @ 127.0.0.1:8080");
