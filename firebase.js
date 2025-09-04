033d6c36ca2cf7cebb074ba8a7818b6cbbb0c312
Shibss1 <Shibss1@shibeyu@gmail.com>
2025-09-04 10:44:16 +0800

    code reorganisation

--- firebase.js
+++ firebase.js
@@ -1,5 +1,5 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('./config/hacka-35045-firebase-adminsdk-fbsvc-3081425674.json');
+const admin = require("firebase-admin");
+const serviceAccount = require("./fb_key.json"); //change to your own firebase service account key
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
